4a7cbbff1a1db34fe2e2f6642a1850093f4632a5
Nipun <nipdeept@gmail.com>
2026-05-18 00:14:11 +0530

    Setting up the game skeleton

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include "raylib.h"
+
+//We define our screen size as symbolic constants at the top
+#define SCREEN_W 800
+#define SCREEN_H 450
 
 int main(void) {
-    printf("Hello, World!\n");
+    //Initialisation
+    InitWindow(SCREEN_W, SCREEN_H, "PONG // NEON EDITION");
+
+    SetTargetFPS(60);
+    //GAME LOOP
+    //WindowShouldClose() returns true if the user presses ESC or the 'X' button
+    while (!WindowShouldClose()) {
+        //UPDATE
+
+        //DRAW
+        BeginDrawing();
+
+        ClearBackground((Color){3, 3, 18, 255});
+
+        //Neon graphics to be drawn here
+        EndDrawing();
+    }
+    //De-initialisation -> cleans up memory and closes the window safely
+    CloseWindow();
     return 0;
 }
