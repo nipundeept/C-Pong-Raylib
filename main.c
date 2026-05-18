9bc2a983548804e77bec75c041fbc0e444f4d19f
Nipun <nipdeept@gmail.com>
2026-05-18 09:51:49 +0530

    Added logic header and initialised speed, ball velocity, player controls, updating the ball

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include "raylib.h"
 #include "types.h" //Includes new custom types
-
-//We define our screen size as symbolic constants at the top
-#define SCREEN_W 800
-#define SCREEN_H 450
+#include "logic.h" //Includes logic menu
 
 int main(void) {
     //Initialisation
@@ -16,17 +13,30 @@ int main(void) {
     Paddle leftPaddle = {0};
     leftPaddle.rec = (Rectangle){ 50, SCREEN_H/2.0f - 50, 18, 100}; // x, y, width, height
     leftPaddle.col = (Color){ 0, 230, 255, 255}; //Cyan
+    leftPaddle.speed = 430.0f; //Give paddles speed
 
     Paddle rightPaddle = {0};
     rightPaddle.rec = (Rectangle){ SCREEN_W - 68, SCREEN_H/2.0f - 50, 18, 100};
-    rightPaddle.col = (Color){ 255, 20, 200, 255}; // Magenta
+    rightPaddle.col = (Color){ 255, 20, 200, 255}; //Magenta
+    rightPaddle.speed = 430.0f; //Give paddles speed
 
     Ball ball = {0};
     ball.pos = (Vector2) {SCREEN_W / 2.0f, SCREEN_H / 2.0f}; //Center of the screen
     ball.radius = 10;
+    ball.spd = (Vector2) { 320.0f, 200.0f}; //give ball starting velocity
     //WindowShouldClose() returns true if the user presses ESC or the 'X' button
     while (!WindowShouldClose()) {
-        //UPDATE
+        float dt = GetFrameTime();
+        //---UPDATE---
+        //WE CALL OUR LOGIC FUNCTIONS
+        //Player 1 controls (W and S)
+        MovePaddle(&leftPaddle, IsKeyDown(KEY_W), IsKeyDown(KEY_S), dt);
+
+        //Player 2 controls (Up Arrow and Down Arrow)
+        MovePaddle(&rightPaddle, IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN), dt);
+
+        //Update the ball
+        UpdateBall(&ball, &leftPaddle, &rightPaddle, dt);
 
         //DRAW
         BeginDrawing();
