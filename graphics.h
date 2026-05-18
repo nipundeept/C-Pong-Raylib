db3f3d75c863f1443429de1f37d891936aac90c2
Nipun <nipdeept@gmail.com>
2026-05-18 10:08:23 +0530

    Added header file for graphics

---
+++ graphics.h
@@ -0,0 +1,13 @@
+//
+// Created by Nipun Deept on 18/05/26.
+//
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include "types.h"
+
+//We pass in our game objects so this file knows what to draw
+void DrawGame(Paddle *left, Paddle *right, Ball *ball);
+
+#endif //GRAPHICS_H
